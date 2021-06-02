a235c4f6e3f3ea58aac53a0420921431b97995a8
mohammadreza Elmi <mrelmimiyab@gmail.com>
2021-06-02 13:38:49 +0430

    Add some changes for run

--- data_gen.py
+++ data_gen.py
@@ -8,6 +8,14 @@ from torchvision import transforms
 from config import IMG_DIR
 from config import pickle_file
 
+
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+import sys 
+import re 
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
@@ -70,15 +78,55 @@ data_transforms = {
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
 }
+DEFAULT_ENCODING = 'utf-8'
+def ustr(x):
+    '''py2/py3 unicode helper'''
 
+    if sys.version_info < (3, 0, 0):
+        from PyQt4.QtCore import QString
+        if type(x) == str:
+            return x.decode(DEFAULT_ENCODING)
+        if type(x) == QString:
+            #https://blog.csdn.net/friendan/article/details/51088476
+            #https://blog.csdn.net/xxm524/article/details/74937308
+            return unicode(x.toUtf8(), DEFAULT_ENCODING, 'ignore')
+        return x
+    else:
+        return x
+
+def natural_sort(list, key=lambda s: s):
+  """
+  Sort the list into natural alphanumeric order.
+  """
+
+  def get_alphanum_key_func(key):
+    convert = lambda text: int(text) if text.isdigit() else text
+    return lambda s: [convert(c) for c in re.split('([0-9]+)', key(s))]
+
+  sort_key = get_alphanum_key_func(key)
+  list.sort(key=sort_key)
+
+
+def scanAllImages(folderPath):
+  extensions = ['.%s' % fmt.data().decode("ascii").lower() for fmt in QImageReader.supportedImageFormats()]
+  images = []
+
+  for root, dirs, files in os.walk(folderPath):
+    for file in files:
+      if file.lower().endswith(tuple(extensions)):
+        relativePath = os.path.join(root, file)
+        path = ustr(os.path.abspath(relativePath))
+        images.append(path)
+  natural_sort(images, key=lambda x: x.lower())
+  return images
 
 class ArcFaceDataset(Dataset):
     def __init__(self, split):
-        with open(pickle_file, 'rb') as file:
-            data = pickle.load(file)
+        # with open(pickle_file, 'rb') as file:
+        #     data = pickle.load(file)
 
         self.split = split
-        self.samples = data
+        # self.samples = data
         self.transformer = data_transforms['train']
 
 
@@ -87,6 +135,7 @@ class ArcFaceDataset(Dataset):
         
         path = self.images_path[i]
         # filename = sample['img']
+        filename = path
         label = int(os.path.basename(os.path.dirname(path)))
 
         # filename = os.path.join(IMG_DIR, filename)
@@ -96,4 +145,4 @@ class ArcFaceDataset(Dataset):
         return img, label
 
     def __len__(self):
-        return len(self.samples)
+        return len(self.images_path)
